b06f292c2f1ca66b36852ad941e3f0f90d9275ef
rdpubana17 <rdpubana16@gmail.com>
2025-04-10 10:44:22 +0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  description = "AWS region to deploy the infrastructure"
+  default     = "us-east-1"
+}
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
@@ -5,19 +10,30 @@ variable "vpc_cidr" {
 
 variable "public_subnet_cidrs" {
   description = "CIDR blocks for public subnets"
-  default     = ["10.0.0.0/24", "10.0.1.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
-  default     = ["10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "instance_type" {
-  description = "Type of the EC2 instances"
+variable "ec2_instance_count" {
+  description = "Number of EC2 instances to provision"
+  default     = 2
+}
+
+variable "ec2_instance_type" {
+  description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
-variable "aws_region" {
-  description = "AWS region to deploy the infrastructure"
-  default     = "us-east-1"
+
+variable "ec2_ami" {
+  description = "AMI ID for EC2 instances"
+  default     = "ami-0f9de6e2d2f067fca" # Replace with a valid AMI ID
+}
+
+variable "alb_security_groups" {
+  description = "Security groups for the Application Load Balancer"
+  default     = [] # Add security group IDs here
 }
